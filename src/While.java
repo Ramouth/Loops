594c0944d2149c861f5e8016894f978036c6a329
Ramouth <42993992+Ramouth@users.noreply.github.com>
2018-10-26 16:37:38 +0200

    Opgaven længere fremme

--- src/While.java
+++ src/While.java
@@ -3,17 +3,18 @@ public class While {
 public static void main(String[] args) {
 
     // WHILE LOOP SOM COUNT DOWN
-    System.out.println("While Loop");
+    // Vi starter vores count med 5
     int x = 5;
-    if(x = 0){
-        System.out.println("Så går vi i gang");
-    }
-    while(x > 0) {
-        System.out.println("Current x:" + x);
-        x = x - 1;
-    }
-    System.out.println("Så går vi i gang!: " + x);
-    System.out.println();
 
-}
+    // Her bruger vi et if statement, der viser at vi er i gang. Conditionen if x er over 0 betyder
+    // at den vil være sand i starten af countdownet
+    if(x > 0) {
+        System.out.println("Nedtællingen begynder"); //Vi får informationen at vi er i gang
+    }
+    while(x >= 0) { // her har vi sat condition, at hvis countdownet er i gang, får vi at vide, hvor langt vi er
+        System.out.println("Vi er på:" + x); // vi får infomation om, hvor langt vi er, hver gang løkken er er i gang
+        x = x - 1; //Her sætter vi countdownet til at gå ned med en
+        }
+    System.out.println("Nedtællingen er slut"); // Vi giver besked om, at nedtællingen er slut
+    }
 }
