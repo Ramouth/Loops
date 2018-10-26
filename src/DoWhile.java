fc196d7e7585d579ed3c976afa49a37d18c86b5d
Ramouth <42993992+Ramouth@users.noreply.github.com>
2018-10-26 17:02:23 +0200

    + Do while løkker i ny class fil

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -1,18 +1,18 @@
-import java.util.Scanner;
+import java.util.Scanner; //importere en scanner
 
 public class DoWhile {
     public static void main(String[] args) {
+        System.out.println("Count Down i Do While Loop"); //Fortæller, hvad programmet gør
 
-       Scanner se = new Scanner(System.in);
+       Scanner se = new Scanner(System.in); //Laver en scanner
 
-    // DO WHILE
+    // Vi forklare brugeren, at vi skal bruge et tal
        System.out.println("Hvor skal vi starte?");
-    int y = se.nextInt();
+    int y = se.nextInt(); //her bruger vi scanneren
        do {
-        System.out.println("Current y: " + y);
-        y = y - 1;
-    } while(y > 0);
-       System.out.println("Final y: "+ y);
-       System.out.println();
+        System.out.println("Tallet er: " + y); // Så længe programmet kører skal det fortælle os, hvor langt vi er
+        y = y - 1; //Tallet bliver mindre hver gang løkken kører.
+    } while(y > 0); //løkken slutter når vi rammer 0
+       System.out.println("Nu er det slut"); //Når vi slutter får brugeren besked
 }
 }
