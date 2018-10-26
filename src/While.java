093050fcb39065b895fb8e0f6489843ac712656c
Ramouth <42993992+Ramouth@users.noreply.github.com>
2018-10-26 16:05:57 +0200

    Første commit med løkker

---
+++ src/While.java
@@ -0,0 +1,19 @@
+public class While {
+
+public static void main(String[] args) {
+
+    // WHILE LOOP SOM COUNT DOWN
+    System.out.println("While Loop");
+    int x = 5;
+    if(x = 0){
+        System.out.println("Så går vi i gang");
+    }
+    while(x > 0) {
+        System.out.println("Current x:" + x);
+        x = x - 1;
+    }
+    System.out.println("Så går vi i gang!: " + x);
+    System.out.println();
+
+}
+}
