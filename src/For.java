1dc058805bdfcd69492fdba663d16e3719edb698
Ramouth <42993992+Ramouth@users.noreply.github.com>
2018-10-26 17:08:32 +0200

    + Ny fil med for loops

---
+++ src/For.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class For {
+    public static void main(String[] args) {
+        Scanner se = new Scanner(System.in);
+        System.out.println("For Loop");
+        for(int i = 3; i > 0; i--) {
+        System.out.println("Current i: " + i);
+    }
+}
+}
